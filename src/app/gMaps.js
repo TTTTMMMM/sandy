d528cf1f2c73f3f031ff52b649fef034a7e3292d
TTTTMMMM <junque135@gmail.com>
2019-08-16 20:56:31 -0400

    Map is all working -- the infoWindow shows up when the icons are clicked. Cool!

--- src/app/gMaps.js
+++ src/app/gMaps.js
@@ -2,13 +2,211 @@ import './../lib/styles/style.css';
 import './../lib/styles/styleGMaps.css';
 import '../lib/scripts/headerStyling.js';
 import '../lib/scripts/footerStyling.js';
+import {api_key} from '../lib/scripts/apikey.js';
+import {rentalAgents} from '../lib/scripts/rentalAgents.js';
+import {rentalAgentsKeyedByBeach} from '../lib/scripts/rentalAgents.js';
+import {theIWArray} from '../lib/scripts/rentalAgents.js';
 
+// console.log(`${JSON.stringify(rentalAgentsKeyedByBeach["Ocean City, MD"])}`);
+console.log(theIWArray["Ocean City, MD"]);
+
+
+let myStyle = [
+    {
+        "featureType": "administrative",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#444444"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#f2f2f2"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 45
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#1b7499"
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    }
+];
 
 var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
-  });
+var vabeach = {lat: 36.50, lng: -75.9};
+var ocmd = {lat: 38.5, lng: -75.04};
+var joiseyShore = {lat: 39.41, lng: -74.60};
+var ncBeaches = {lat: 34.00, lng: -78.35};
+
+var EAST_COAST_BOUNDS = {
+    north: 42.0,
+    south: 30.0,
+    west: -80.00,
+    east: -72.00,
+};
+
+function CenterControl(controlDiv, map, cityName, latlng, zLevel) {
+    let controlUI = document.createElement('div');
+    controlUI.classList.add("dOuter");
+    controlDiv.appendChild(controlUI); 
+    
+    let controlText = document.createElement('div');
+    controlText.classList.add("dInner");
+    controlText.innerHTML = cityName;
+    controlText.style.fontFamily = "Josefin Sans, monotype";
+    controlUI.appendChild(controlText);
+  
+    controlUI.addEventListener('click', function() {
+        map.panTo(latlng);
+        map.setZoom(zLevel);
+    });
 }
+  
+function anotherControl(controlDiv) {
+    let controlUI = document.createElement('div');
+    controlUI.classList.add("dInstructionOuter");
+    controlDiv.appendChild(controlUI); 
+    
+    let controlText = document.createElement('div');
+    controlText.classList.add("dInstructionInner");
+    controlText.innerHTML = `Zoom in on beach and click on icons for rental info.`;
+    controlText.style.fontFamily = "Josefin Sans, monotype";
+    controlUI.appendChild(controlText);
+}
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 9,
+        center: ocmd,
+        styles: myStyle,
+        mapTypeControl: false,
+        streetViewControl: false,
+        restriction: {
+        latLngBounds: EAST_COAST_BOUNDS,
+        strictBounds: false,
+        },
+    });
+
+    let centerControlDiv1 = document.createElement('div');
+    CenterControl(centerControlDiv1, map, 'Jersey Shore', joiseyShore, 9 );
+
+    let centerControlDiv2 = document.createElement('div');
+    CenterControl(centerControlDiv2, map, 'Maryland/Delaware', ocmd, 10 );
+    
+    let centerControlDiv3 = document.createElement('div');
+    CenterControl(centerControlDiv3, map, 'Virginia Beach/OBX', vabeach, 9 );
+    
+    let centerControlDiv4 = document.createElement('div'); 
+    CenterControl(centerControlDiv4, map, 'Carolina Beaches', ncBeaches, 9 );
+    
+    let instructionControlDiv = document.createElement('div');
+    anotherControl(instructionControlDiv);
+    
+    map.controls[google.maps.ControlPosition.LEFT_TOP].push(centerControlDiv1);
+    map.controls[google.maps.ControlPosition.LEFT_TOP].push(centerControlDiv2);
+    map.controls[google.maps.ControlPosition.LEFT_TOP].push(centerControlDiv3);
+    map.controls[google.maps.ControlPosition.LEFT_TOP].push(centerControlDiv4);
+    
+    map.controls[google.maps.ControlPosition.TOP_CENTER].push(instructionControlDiv);
+
+    let url = `./lib/images/longMascotIcon.png`;
+        let image = {
+        url: url, 
+        scaledSize: new google.maps.Size(38, 38),
+    };
+
+    /* put a marker on the map for each location, using the SH logo  */
+    rentalAgents.forEach(agent => agent.locations.forEach(loc => {
+        /* create a marker with the SH logo at every location based on rentalAgents[] */
+        let rentMarker = new google.maps.Marker({
+            position: new google.maps.LatLng(loc.location),
+            map: map,
+            icon: image,
+        })
+
+        /* create an infoWindow, which shows when the markers are clicked  */
+        let contentString = `${JSON.stringify(rentalAgentsKeyedByBeach[loc.beach])}`;
+        contentString = theIWArray[loc.beach];
+        let iw = new google.maps.InfoWindow({
+                content: contentString,
+            });
+        
+        /* create the listener for each marker, which displays the infoWindow content */
+        rentMarker.addListener('click', function () {
+            iw.open(map, rentMarker);
+        })
+    })); 
+  
+    map.addListener('zoom_changed', function () {
+        if(map.getZoom() < 6) {
+            map.setZoom(6);
+            }
+            if(map.getZoom() > 13) {
+            map.setZoom(13);
+            }
+    });
+}
+
+// Do not delete! //
 window.initMap = initMap;
+// Do not delete! //
---
+++ src/lib/scripts/rentalAgents.js
@@ -0,0 +1,320 @@
+export const rentalAgents = [
+	{
+        "agent": "Sand Helper",
+        "phone": "(855) 743-5737",
+        "email": "rent@sandhelper.com",
+        "url": "http://sandhelper.com",
+        "notes": "",
+        "locations": [
+            {
+            "beach": "Ocean City, MD",
+            "location": {lat: 38.3365, lng: -75.0849},
+            },
+            {
+            "beach": "Fenwick",
+            "location": {lat: 38.4623, lng: -75.0513},
+            },   
+            {
+            "beach": "Bethany",
+            "location": {lat: 38.5395, lng: -75.0552},
+            }, 
+            {
+            "beach": "Assateague",
+            "location": {lat: 38.0917, lng: -75.2038},
+            }, 
+   		]
+    },
+   
+   {
+        "agent": "Grand Rental Station",
+        "phone": "(302) 227-7368",
+        "email": "info@227rent.com",
+  	    "url": "https://www.227rent.com/shop/vacation-items/beach-wheelchair-770.html",
+        "notes": "",
+        "locations": [
+            {
+            "beach": "Rehoboth",
+            "location": {lat: 38.7168, lng: -75.0760},
+            },
+            {
+            "beach": "Lewes",
+            "location": {lat: 38.7745, lng: -75.1393},
+            },   
+            {
+            "beach": "Dewey",
+            "location": {lat: 38.6929, lng: -75.0746},
+            }, 
+   		]
+    },
+   
+   {
+        "agent": "Stone Harbor Crib & Beach Rental",
+        "phone": "(609) 368-7271",
+        "notes": "",
+        "email": "info@stoneharborcribrental.com",
+        "url": "http://www.stoneharborcribrental.com/Beach-Equipment/",
+        "locations": [
+                {
+                "beach": "Stone Harbor",
+                "location": {lat: 39.0509, lng: -74.7579},
+                },
+                {
+                "beach": "Cape May",
+                "location": {lat: 38.9593, lng: -74.9274},
+                },   
+                {
+                "beach": "Avalon",
+                "location": {lat: 39.0918, lng: -74.7365},
+                }, 
+                            {
+                "beach": "Sea Isle City ",
+                "location": {lat: 39.1526, lng: -74.6974},
+                },
+                {
+                "beach": "Wildwood",
+                "location": {lat: 38.9890, lng: -74.8199},
+                },   
+                {
+                "beach": "Ocean City, NJ",
+                "location": {lat: 39.2776, lng: -74.5746},
+                }, 
+            ]
+    },
+   
+   {
+        "agent": "Beach Easy Rentals",
+        "phone": "(609) 234-4698 ",
+        "notes": "",
+        "email": "kim@beacheasynj.com",
+        "url": "https://beacheasynj.com/",
+        "locations": [
+            {
+                "beach": "Long Beach Island",
+                "location": {lat: 39.6598, lng: -74.1708},
+            },
+            {
+                "beach": "Atlantic City",
+                "location": {lat: 39.3784, lng: -74.4233},
+            },   
+            {
+                "beach": "Seaside Heights",
+                "location": {lat: 39.9443, lng: -74.0729},
+            }, 
+            {
+                "beach": "Ocean City, NJ",
+                "location": {lat: 39.2776, lng: -74.5746},
+            },
+        ]
+    },
+   
+   {
+        "agent": "Beach Power Mobility",
+        "phone": "(757) 481-1590",
+        "notes": "",
+        "email": "info@aeromedic.com",
+        "url": "https://beachpowermobility.rezdy.com/catalog/282257/sandhelper",
+        "locations": [
+            {
+                "beach": "Virginia Beach",
+                "location": {lat: 36.8529, lng: -75.97798},
+            },
+        ]
+    },
+   
+    {
+        "agent": "Just for Beach Rentals",
+        "phone": "(866) 629-7368",
+        "notes": "",
+        "email": "rentals@justforthebeach.com",
+        "url": "https://justforthebeach.com/product/electric-beach-wheel-chair/",
+        "locations": [
+            {
+                "beach": "OBX (Corolla to Nags Head)",
+                "location": {lat: 36.1696, lng: -75.7552},
+            },
+        ]
+    },
+
+    {
+        "agent": "Julie's Rentals",
+        "phone": "(910) 579-1211",
+        "email": "customerservice@juliesrentals.com",
+        "url": "https://juliesrentals.com/beach-gear-rentals-umbrella-rentals-beach-wagon/",
+        "notes": "",
+        "locations": [
+            {
+            "beach": "Sunset Beach",
+            "location": {lat: 33.8807, lng: -78.5122},
+            },
+            {
+            "beach": "Ocean Isle Beach",
+            "location": {lat: 33.8943, lng: -78.4267},
+            },   
+            {
+            "beach": "Holden Beach",
+            "location": {lat: 33.91416, lng: -78.30696},
+            }, 
+            {
+            "beach": "North Myrtle Beach",
+            "location": {lat: 33.81809, lng: -78.67997},
+            }, 
+        ]
+    },
+
+    {
+        "agent": "Coastal Golf Cart Rentals",
+        "phone": "(910) 477-0716",
+        "notes": "",
+        "email": "missy@coastalgolfcartrentals.com",
+        "url": "https://www.coastalgolfcartrentals.com/Power-Beach-Wheelchair-Rentals",
+        "locations": [
+            {
+            "beach": "Ocean Isle Beach",
+            "location": {lat: 33.8943, lng: -78.42667},
+            },
+            {
+            "beach": "Wilmington",
+            "location": {lat: 34.2085, lng: -77.79637},
+            },   
+            {
+            "beach": "Southport",
+            "location": {lat: 33.92156, lng: -78.02027},
+            }, 
+                        {
+            "beach": "Holden Beach",
+            "location": {lat: 33.91416, lng: -78.30696},
+            },
+            {
+            "beach": "Sunset Beach",
+            "location": {lat: 33.8807, lng: -78.5122},
+            },   
+            {
+            "beach": "Leland",
+            "location": {lat: 34.25628, lng: -78.04471},
+            }, 
+        ]
+    },
+
+    {
+        "agent": "Mobility Center of the Grand Strand",
+        "phone": "(843) 692-8001",
+        "notes": "",
+        "email": "info@mobility-center.com",
+        "url": "http://mobility-center.com/equipment-rentals/",
+        "locations": [
+           {
+              "beach": "Myrtle Beach",
+              "location": {lat: 33.68906, lng: -78.88669},
+           },
+        ]
+     },
+
+     {
+        "agent": "Dandy Don's",
+        "phone": "410-289-2289",
+        "notes": "Hourly rentals only; for daily/weekly deliveries, call Sand Helper 855-743-5737",
+        "email": "contact@ocmdbikerentals.com",
+        "url": "http://ocmdbikerentals.com/index.html",
+        "locations": [
+           {
+              "beach": "Ocean City, MD",
+              "location": {lat: 38.3365, lng: -75.0849},
+           },
+        ]
+     },
+   
+];
+
+let tempLocationRentals = [];
+rentalAgents.forEach(agent => agent.locations.forEach(loc => {
+   let locRentalObject = {};
+   locRentalObject.beach = loc.beach;
+   locRentalObject.rentalAgent = agent.agent;
+   locRentalObject.phone = agent.phone;
+   locRentalObject.notes = agent.notes;
+   locRentalObject.email = agent.email;
+   locRentalObject.url = agent.url; 
+   tempLocationRentals.push(locRentalObject);
+}));
+
+export let rentalAgentsKeyedByBeach = {};
+tempLocationRentals.map(locR => {
+	if(rentalAgentsKeyedByBeach.hasOwnProperty(locR.beach)) {
+   	    let skinnyLocR = {};
+        Object.assign(skinnyLocR, locR);  // Object.assign(dest, src);
+        delete skinnyLocR.beach;            // don't need the name of the beach since it's the key of this array
+        rentalAgentsKeyedByBeach[locR.beach].push(skinnyLocR);
+   } else {
+   	    let skinnyLocR = {};
+   	    Object.assign(skinnyLocR, locR);
+        delete skinnyLocR.beach;
+        rentalAgentsKeyedByBeach[locR.beach] = [skinnyLocR]; // note: this is an array of 1 object, not just an object
+   }
+});
+//console.log(`${JSON.stringify(rentalAgentsKeyedByBeach["Ocean City, MD"])}`);
+//console.log(`${Object.keys(rentalAgentsKeyedByBeach["Ocean City, MD"]).length}`);
+
+/* make the html div element that will be displayed by google maps when a SH icon is clicked */
+let theBeaches = Object.keys(rentalAgentsKeyedByBeach);
+
+export let theIWArray = [];
+theBeaches.forEach(b => {
+    let theIWDiv = document.createElement("div");
+    theIWDiv.classList.add("IWdiv");
+    let theBeachName = document.createElement("p");
+    theBeachName.classList.add("IWp");
+    theBeachName.innerHTML = b;
+    theIWDiv.appendChild(theBeachName);
+    theIWDiv.style.fontFamily = "Josefin Sans, monotype";
+    let theRentalAgentsList = document.createElement("ul");
+    theRentalAgentsList.classList.add("IWul");
+    theIWDiv.appendChild(theRentalAgentsList);
+    rentalAgentsKeyedByBeach[b].forEach(ra => {
+        let theRentalAgentsListItemName = document.createElement("li");
+        theRentalAgentsListItemName.classList.add("IWliName");
+        theRentalAgentsListItemName.style.fontFamily = "Josefin Sans, monotype";
+        theRentalAgentsListItemName.innerHTML = ra.rentalAgent;
+        theRentalAgentsList.appendChild(theRentalAgentsListItemName);
+
+        let theRentalAgentsListItemPhone = document.createElement("li");
+        theRentalAgentsListItemPhone.classList.add("IWliPhone");
+        theRentalAgentsListItemPhone.style.fontFamily = "Josefin Sans, monotype";
+        theRentalAgentsListItemPhone.innerHTML = ra.phone;
+        theRentalAgentsList.appendChild(theRentalAgentsListItemPhone);
+
+        let theRentalAgentsListItemEmail = document.createElement("li");
+        let theEmailLink = document.createElement("a");
+        theEmailLink.setAttribute('href', "mailto:" + ra.email);
+        theEmailLink.innerHTML = ra.email;
+        theRentalAgentsListItemEmail.appendChild(theEmailLink);
+        theRentalAgentsListItemEmail.classList.add("IWliEmail");
+        theRentalAgentsListItemEmail.style.fontFamily = "Josefin Sans, monotype";
+        theRentalAgentsList.appendChild(theRentalAgentsListItemEmail);
+
+        let theRentalAgentsListItemURL = document.createElement("li");
+        let theWebsiteLink = document.createElement("a");
+        theWebsiteLink.setAttribute('href', ra.url);
+        theWebsiteLink.innerHTML = "Website";
+        theRentalAgentsListItemURL.appendChild(theWebsiteLink);
+        theRentalAgentsListItemURL.classList.add("IWliURL");
+        theRentalAgentsListItemURL.style.fontFamily = "Josefin Sans, monotype";
+        theRentalAgentsList.appendChild(theRentalAgentsListItemURL);
+
+        let theRentalAgentsListItemNotes = document.createElement("li");
+        theRentalAgentsListItemNotes.classList.add("IWliNotes");
+        theRentalAgentsListItemNotes.style.fontFamily = "Josefin Sans, monotype";
+        theRentalAgentsListItemNotes.innerHTML = ra.notes;
+        theRentalAgentsList.appendChild(theRentalAgentsListItemNotes);
+    })
+    theIWArray[b] = theIWDiv.outerHTML;
+    //console.log(theIWDiv.outerHTML);
+});
+
+// console.log(Object.keys(theIWArray));
+// console.log(theIWArray["Ocean City, MD"]);
+/* theIWArray.forEach( x => {
+    console.log(x);
+}); */
+
+
+
